3af1cfcd6fd73c9e08f0e5f9cb29060c44482820
Peteris Krumins <peteris.krumins@gmail.com>
2013-04-25 23:49:29 +0300

    it works. deployed in production

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <cstdio>
+
+int main() {
+    while (1) {
+        HWND dialog = FindWindow("#32770", "Google Chrome");
+        if (dialog) {
+            printf("Found dialog: %x\n", dialog);
+            HWND dialogItem = NULL;
+            while (1) {
+                dialogItem = FindWindowEx(dialog, dialogItem, NULL, NULL);
+                if (dialogItem == 0) {
+                    break;
+                }
+                char windowText[255];
+                GetWindowText(dialogItem, windowText, 255);
+                if (strcmp(windowText, "OK") == 0) {
+                    SetActiveWindow(dialog);
+                    SendMessage(dialogItem, BM_CLICK, NULL, NULL);
+                    break;
+                }
+            }
+        }
+        Sleep(1000);
+    }
+}
